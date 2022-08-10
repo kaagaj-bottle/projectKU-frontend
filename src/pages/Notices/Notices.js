8452a6db441c39c74673deacadbc5c7a0db18a3c
Zishan <zishansiddique80@gmail.com>
2022-08-10 08:29:21 +0545

    remove unncessary and tad bit of styling

--- src/pages/Notices/Notices.js
+++ src/pages/Notices/Notices.js
@@ -9,7 +9,9 @@ import Paper from "@mui/material/Paper";
 import { useState, useEffect } from "react";
 import noticeService from "../../services/notices";
 import { nanoid } from "nanoid";
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { Box } from "@mui/system";
 
 function Notices() {
   const [notices, setNotices] = useState([]);
@@ -25,84 +27,61 @@ function Notices() {
     window.open(pdfUrl);
   };
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{}} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Title</TableCell>
-            <TableCell align="center">Date</TableCell>
-            <TableCell align="center">Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {notices.map((row) => {
-            const formattedDate = new Date(row.date);
-            return (
+    <Grid container justifyContent={"center"}>
+      <Grid item xs={12} md={11} sx={{ p: 2 }}>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
               <TableRow
-                key={nanoid()}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                sx={{
+                  "&:last-child td, &:last-child th": {
+                    fontSize: "1.2rem",
+                    fontWeight: 700,
+                  },
+                }}
               >
-                <TableCell component="th" scope="row">
-                  {row.title}
-                </TableCell>
-                <TableCell align="center">
-                  {formattedDate.toLocaleDateString()}
-                </TableCell>
-                <TableCell align="center">
-                  <Button
-                    variant="contained"
-                    onClick={(event) => handleViewPdf(event, row.pdfUrl)}
-                  >
-                    View
-                  </Button>
-                </TableCell>
+                <TableCell align="left">Title</TableCell>
+                <TableCell align="center">Date</TableCell>
+                <TableCell align="center">Action</TableCell>
               </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            </TableHead>
+
+            <TableBody>
+              {notices.map((row) => {
+                const formattedDate = new Date(row.date);
+                return (
+                  <TableRow
+                    key={nanoid()}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                      ":hover": {
+                        bgcolor: grey[300],
+                      },
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.title}
+                    </TableCell>
+                    <TableCell align="center">
+                      {formattedDate.toLocaleDateString()}
+                    </TableCell>
+                    <TableCell align="center">
+                      <Button
+                        variant="contained"
+                        onClick={(event) => handleViewPdf(event, row.pdfUrl)}
+                      >
+                        View
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 }
-
-// import { getAllByAltText } from "@testing-library/react";
-// import React, { useEffect, useState } from "react";
-// import NoticeCard from "../../components/CustomCards/NoticeCard";
-// import noticeService from "../../services/notices";
-// import { nanoid } from "nanoid";
-// import { Grid } from "@mui/material";
-// const Notices = () => {
-//   const [notices, setNotices] = useState([]);
-
-//   useEffect(() => {
-//     noticeService.getAll().then((notices) => {
-//       setNotices(notices);
-//     });
-//   }, []);
-
-//   console.log(notices);
-//   const handleViewPdf = (url, event) => {
-//     event.preventDefault();
-//     window.open(url);
-//   };
-
-//   return (
-//     <>
-//       <Grid container gap={2} justifyContent="center" sx={{ mt: 3 }}>
-//         {notices.map((notice) => {
-//           return (
-//             <NoticeCard
-//               key={nanoid()}
-//               title={notice.title}
-//               pdfUrl={notice.pdfUrl}
-//               date={notice.date}
-//               handleViewPdf={handleViewPdf}
-//             />
-//           );
-//         })}
-//       </Grid>
-//     </>
-//   );
-// };
 
 export default Notices;
