2f3d5e86a99023619f0bca72f9af49afbbcd75ca
Zishan <zishansiddique80@gmail.com>
2022-08-11 18:24:23 +0545

    changed base url for fetching data

--- src/services/notices.js
+++ src/services/notices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/notices";
+const baseUrl = "http://localhost:3001/api/notices";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
